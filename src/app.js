d216cfa7b8b2af20e2cc0d1bd4d6d8fa0ec5c753
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-08-06 23:36:30 +0100

    feat(class-basic): class properties and instance properties [delivers]

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('in app.js')
+class Drone {
+    constructor(id, name) {
+        //instance properties
+        this.id = id;
+        this.name = name;
+    }
+}
+// class properties
+Drone.maxHeight = 2000;
+
+let drone = new Drone('A123', 'Flyer');
+let drone2 = new Drone('A12', 'Flyinh');
+console.log(Drone.maxHeight);
