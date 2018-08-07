684029b8d0834357d2939b54bf06b72c6732a302
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-08-07 18:15:36 +0100

    feay(data-service): populating classes [delivers]

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import {Drone} from './classes/drone.js';
 import {fleet} from './fleet-data.js';
 import {FleetDataService} from './services/fleet-data-service.js';
 
-let dataService = new FleetDataService
-dataService.loadDate(fleet);
-
-console.log(dataService.cars);
+let dataService = new FleetDataService();
+dataService.loadData(fleet);
+for (let car of dataService.cars)
+    console.log(car.license);
