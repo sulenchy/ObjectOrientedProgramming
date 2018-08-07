2e811b6f473060d9a116f8d06526deff41412876
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-08-07 11:45:40 +0100

    feat(inheritance-js-implementation): inheriting constructors

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-console.log('in app.js')
+class Vehicle{
+    constructor (licenseNum) {
+        this.licenseNum = licenseNum;
+    }
+}
+
+
+class Drone extends Vehicle{
+
+}
+
+class Car extends Vehicle{
+    constructor (licenseNum) {
+        super(licenseNum);
+    }
+}
+
+
+let c = new Car('A123');
+
+// console.log(c instanceof Object);
+
+console.log(c.licenseNum);
