325b1c30b9b71d280738da0a7ee166c5a78e8330
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-08-07 11:57:28 +0100

    feat(inheritance-js-implementation): inheriting method

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
 class Vehicle{
-    constructor () {
-        this.gpsEnabled = true;
+    start(){
+        console.log('starting Vehicle');
+    }
+
+    static getCompanyName(){
+        console.log('My Company')
     }
 }
 
 
-class Drone extends Vehicle{
-
-}
-
 class Car extends Vehicle{
-    constructor () {
-        super();
-        this.gpsEnabled = false;
+    start(){
+        super.start();
+        console.log('starting Car');
+    }
+
+    static getCompanyName(){
+        super.getCompanyName();
+        console.log('My Other Company');
     }
 }
 
 
 let c = new Car();
-
-// console.log(c instanceof Object);
-
-console.log(c.gpsEnabled);
+Car.getCompanyName();
